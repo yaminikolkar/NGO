f8e393c0f4c03a2e369a3c24c34163b61d7d7b4d
yaminikolkar <yaminikolkar2006@gmail.com>
2026-01-08 00:33:05 +0530

    Remove Vite configuration file

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,1 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  }
-});
